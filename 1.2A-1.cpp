e31e41be6032f205ae59c39d9ea0561f9a2ffaa4
Jed <51814517+cornishjed@users.noreply.github.com>
2019-09-30 16:50:33 +0100

    Create 1.2A-1.cpp

---
+++ 1.2A-1.cpp
@@ -0,0 +1,27 @@
+// Worksheet 1.2A Exercise 1: Grading Program
+// Input score to output grade
+// By Jed Powell
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int score;
+
+	cout << "Enter your score: ";
+	cin >> score;
+
+	if (score <= 59)
+		cout << "\nGrade F" << endl;
+	else if ((score >= 60) && (score <= 69))
+		cout << "\nGrade D" << endl;
+	else if ((score >= 70) && (score <= 79))
+		cout << "\nGrade C" << endl;
+	else if ((score >= 80) && (score <= 89))
+		cout << "\nGrade B" << endl;
+	else if ((score >= 90) && (score <= 100))
+		cout << "\nGrade A" << endl;
+
+	return 0;
+}
